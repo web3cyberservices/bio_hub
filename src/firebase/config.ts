645645ab6d7288b6beaaa3b8b265748d7dfe99a6
Web3CyberServices <web3cyberservices@gmail.com>
2026-04-16 21:46:13 +0000

    Ошибка при ключа, куда ввести новый?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,13 @@
+/**
+ * Конфигурация Firebase.
+ * Эти данные автоматически заполняются при нажатии кнопки "Connect to Firebase" в Studio.
+ * Если вы вводите их вручную, возьмите их из настроек проекта в консоли Firebase.
+ */
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "ВАШ_API_KEY",
+  authDomain: "ВАШ_PROJECT_ID.firebaseapp.com",
+  projectId: "ВАШ_PROJECT_ID",
+  storageBucket: "ВАШ_PROJECT_ID.appspot.com",
+  messagingSenderId: "ВАШ_SENDER_ID",
+  appId: "ВАШ_APP_ID",
 };
