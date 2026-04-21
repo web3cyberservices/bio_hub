b9bcec91b561dbb69359d2fead648346269c753c
Web3CyberServices <web3cyberservices@gmail.com>
2026-04-21 19:41:29 +0000

    исправь бодисайз лимит в создать из продуктов

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
   images: {
     remotePatterns: [
       {
