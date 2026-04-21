f3de88e865b1c68eab7ee255d05663edac35073e
Web3CyberServices <web3cyberservices@gmail.com>
2026-04-21 04:52:19 +0000

    const firebaseConfig = {   apiKey: "AIzaSyDiEz34LESJ9pOybZl1XoYiKcVjdNk1

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "webcybersecurity",
-  "appId": "1:631395655738:web:6904688486c71513939503",
   "apiKey": "AIzaSyDiEz34LESJ9pOybZl1XoYiKcVjdNk1uXs",
   "authDomain": "webcybersecurity.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "631395655738"
+  "projectId": "webcybersecurity",
+  "storageBucket": "webcybersecurity.firebasestorage.app",
+  "messagingSenderId": "631395655738",
+  "appId": "1:631395655738:web:6904688486c71513939503"
 };
