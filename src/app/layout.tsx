72f54b5817a617ea46d744cf4d35d004a28fbe7d
Web3CyberServices <web3cyberservices@gmail.com>
2026-07-21 05:52:25 +0000

    убери пока подключение к телеграму

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { FirebaseClientProvider } from "@/firebase/client-provider";
-import { TelegramInit } from "@/components/telegram-init";
-import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -18,7 +16,7 @@ export const viewport: Viewport = {
 
 export const metadata: Metadata = {
   title: "VPN PRO Premium",
-  description: "Secure High-End VLESS VPN Tunnel for Telegram WebApp.",
+  description: "Secure High-End VLESS VPN Tunnel.",
 };
 
 export default function RootLayout({
@@ -28,15 +26,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru" suppressHydrationWarning className="dark">
-      <head>
-        <Script 
-          src="https://telegram.org/js/telegram-web-app.js" 
-          strategy="beforeInteractive" 
-        />
-      </head>
       <body className={`${inter.variable} font-sans bg-[#02040a] text-slate-50 antialiased min-h-screen selection:bg-cyan-500/30`} suppressHydrationWarning>
         <FirebaseClientProvider>
-          <TelegramInit />
           <div className="relative flex min-h-screen flex-col overflow-x-hidden">
             {children}
           </div>
