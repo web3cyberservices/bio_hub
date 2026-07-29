a9902cfea6343c7f436ffb616ba4c647dbc5347b
Web3CyberServices <web3cyberservices@gmail.com>
2026-07-29 12:16:19 +0000

    пропиши нормально гитигнор  studio-8191589019:~/studio{main}$ git push o

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a6a940aa96a82ec3"; }
+/******/ 		__webpack_require__.h = function() { return "7f11db8384821db8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
